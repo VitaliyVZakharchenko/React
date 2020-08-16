37bbbeb4fb8e2b0adac66250f5e5afea91e25f8a
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-16 18:44:32 +0300

    React_lesson_1_task_3

--- lesson_1/task_3/webpack.config.js
+++ lesson_1/task_3/webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js'
+    },
     module: {
       rules: [
         {
