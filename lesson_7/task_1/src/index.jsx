debf29b22f5b6cdfbe4d3d0761d9deca5c54f217
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-08-26 17:03:21 +0300

    React_lesson_7_task_1

---
+++ lesson_7/task_1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import NumbersList from './NumbersList';
+
+const rootElem = document.querySelector('#root');
+
+const numbers = [1, 2, 3, 4, 5];
+
+ReactDOM.render(<NumbersList numbers={numbers} />, rootElem);
