ffaab4fd3819f2c90fea710ebbb26fd87a19699a
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-09-02 10:42:25 +0300

    React_lesson_9_task_2

---
+++ lesson_9/task_2/src/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import UserForm from "./UserForm";
+
+const user = {
+  firstName: "Bob",
+  student: "false",
+  occupation: "Sidney",
+  about: "",
+};
+
+class App extends Component {
+  createUser = (user) => {
+    console.log(user);
+  };
+
+  render() {
+    return <UserForm onSubmit={this.createUser} />;
+  }
+}
+
+export default App;
