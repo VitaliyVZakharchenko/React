dc0d48020ea6ef19c727470631c36b475ad4e1a3
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-09-03 19:28:46 +0300

    React_lesson_10_hw_1

---
+++ lesson_10/hw_2/Users.jsx
@@ -0,0 +1,47 @@
+const users = [
+    {
+      id: "id-0",
+      age: 21,
+      name: "Bob",
+    },
+    {
+      id: "id-1",
+      age: 17,
+      name: "Tom",
+    },
+    {
+      id: "id-2",
+      age: 18,
+      name: "Tad",
+    },
+    {
+      id: "id-3",
+      age: 45,
+      name: "Sam",
+    },
+    {
+      id: "id-4",
+      age: 26,
+      name: "Mack",
+    },
+    {
+      id: "id-5",
+      age: 65,
+      name: "Tad",
+    },
+    {
+      id: "id-6",
+      age: 28,
+      name: "Molly",
+    },
+    {
+      id: "id-7",
+      age: 33,
+      name: "Dolly",
+    },
+    {
+      id: "id-8",
+      age: 25,
+      name: "Gordon",
+    },
+  ];
