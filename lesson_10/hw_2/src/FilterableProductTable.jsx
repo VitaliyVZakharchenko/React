daab3c6cfb43426fecca22df4a57ce1cf35413f3
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-09-04 01:07:29 +0300

    React_lesson_7_hw_2

---
+++ lesson_10/hw_2/src/FilterableProductTable.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
+
+export default class FilterableProductTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: "",
+      inStockOnly: false,
+    };
+  }
+
+  handleFilterTextChange = (filterText) => {
+    this.setState({
+      filterText: filterText,
+    });
+  };
+
+  handleInStockChange = (inStockOnly) => {
+    this.setState({
+      inStockOnly: inStockOnly,
+    });
+  };
+
+  render() {
+    const { filterText, inStockOnly } = this.state;
+
+    return (
+      <div>
+        <SearchBar
+          filterText={filterText}
+          inStockOnly={inStockOnly}
+          onFilterTextChange={this.handleFilterTextChange}
+          onInStockChange={this.handleInStockChange}
+        />
+        <ProductTable
+          filterText={filterText}
+          inStockOnly={inStockOnly}
+          products={this.props.products}
+        />
+      </div>
+    );
+  }
+}
