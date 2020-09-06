bcce3a6cddb042b80aebe44c4351953ce4ae49da
VitaliyZakharchenko <vitaliy.v.zaharchenko@gmail.com>
2020-09-06 17:26:56 +0300

    React_lesson_11_hw_1

---
+++ lesson_11/hw_1/src/Expand.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+class Expand extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  handleDialog = () => {
+    this.state.isOpen
+      ? this.setState({
+          isOpen: false,
+        })
+      : this.setState({
+          isOpen: true,
+        });
+  };
+
+  render() {
+    const { children, title } = this.props;
+
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={this.handleDialog}>
+            {this.state.isOpen ? "↑" : "↓"}
+          </button>
+        </div>
+        {this.state.isOpen ? (
+          <div className="expand__content">{children}</div>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default Expand;
---
+++ lesson_11/task_2/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Dialog from "./Dialog";
+
+class App extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  hideDialog = () => {
+    this.setState({
+      isOpen: false,
+    });
+  };
+
+  showDialog = () => {
+    this.setState({
+      isOpen: true,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <button className="btn" onClick={this.showDialog}>
+          Show dialog
+        </button>
+        <Dialog
+          isOpen={this.state.isOpen}
+          title={"Recommendation"}
+          onClose={this.hideDialog}
+        >
+          {
+            <p>
+              Use immutable array methods to work with data. It will help to
+              avoid bugs
+            </p>
+          }
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default App;
